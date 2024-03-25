3d5796d0886728759826f0aa1344c4d790314697
PradanaB4CKUP <aji2207ok@gmail.com>
2024-03-25 11:12:34 +0700

    upload file

---
+++ src/Persegi.java
@@ -0,0 +1,14 @@
+public class Persegi {
+
+    double Panjang;
+    double Lebar;
+    double Luas;
+
+    public static double HitungPersegi(double Panjang, double Lebar)
+    {
+        return Panjang + Lebar;
+    }
+
+
+    
+}
---
+++ src/Segitiga.java
@@ -0,0 +1,16 @@
+public class Segitiga {
+
+    double sisiSatu;
+    double sisiDua;
+    double sisiTiga;
+
+    public static double HitungKeliling(double sisiSatu, double sisiDua, double sisiTiga)
+    {
+        return sisiSatu + sisiDua + sisiTiga;
+    }
+
+    
+    
+    
+    
+}
